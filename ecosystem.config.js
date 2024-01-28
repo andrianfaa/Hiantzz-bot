329746f076ca253530e3573876c75fec497ea907
andrianfaa <hi@anfa.my.id>
2024-01-28 12:10:01 +0700

    fix: path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,8 @@
-// import path from "node:path";
-const path = require("node:path");
-
 module.exports = {
   apps: [
     {
       name: "Hiantzz-bot",
-      scripts: path.resolve(process.cwd(), "build", "client.js"),
+      scripts: "./build/client.js",
       watch: true,
       exec_mode: "cluster",
       instances: 4
