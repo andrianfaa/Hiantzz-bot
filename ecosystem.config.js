dd78d0dc04fb8c77ec0cfda4a88fb29f4f7bd138
andrianfaa <hi@anfa.my.id>
2024-01-28 12:10:40 +0700

    fix: typo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "Hiantzz-bot",
-      scripts: "./build/client.js",
+      script: "./build/client.js",
       watch: true,
       exec_mode: "cluster",
       instances: 4
