f771b8ff8acbfe17905c49a6638a2db37c567c3c
andrianfaa <hi@anfa.my.id>
2024-02-01 00:48:26 +0700

    update `ecosystem.config.js`

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,7 @@ module.exports = {
   apps: [
     {
       name: "hiantzz",
-      script: "./build/client.js",
-      exec_mode: "cluster",
-      max_memory_restart: "250M",
-      instances: "max"
+      script: "./build/client.js"
     }
   ]
 }
